c9d47962beef6feff0006ca048e9daf8e8443e3f
dmtrung14 <trungdangminh14012004@gmail.com>
2023-06-25 19:23:19 +0700

    edit some css

--- src/components/body/Countdown.js
+++ src/components/body/Countdown.js
@@ -5,7 +5,7 @@ import "../index.css"
 export default function Countdown (props) {
     return (
         <>
-            <div className={props.class}>{props.time} </div>
+            <div className="countdown-timer">{props.time} </div>
         </>
     )
 }

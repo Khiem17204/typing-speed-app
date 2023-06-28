5e9d4851aebeb8a871791431ea3bdb358a0d0e55
Huy Cao <caogiahuy615@gmail.com>
2023-06-28 14:18:49 +0700

    route update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import React, {useState, useEffect} from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Header from './components/Header'
 import Body from "./components/body/Body"
 import Login from "./components/Login"
@@ -12,19 +13,19 @@ function App() {
   const logout = () => setIsLoggedIn(false)
 
   
-  
-  return (
-    <>
     
-   <Header />
-    <Body/>
-    {/* <Login /> 
-    <Result />
-    */}
-    <Footer />
-    </>
-  )
-}
+    return (
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Body />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    );
+  }
 
 
-export default App;
+  export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,13 @@ import React from 'react'
 import {useState, useEffect} from 'react'
 import './index.css'
 import Logo from "../assets/favicon.png"
-
+import { Link } from "react-router-dom";
 export default function Header() {
     return (
         <header className="header">
             <img className="header--logo" src = {Logo} />
-            <h1 className="header--title"> TypingRace</h1>
+            <Link to="/" className="header--title"> TypingRace</Link>
+            <Link to="/login" className='user-logo'><i class="fa-solid fa-user"></i></Link>
         </header>
     )
 }

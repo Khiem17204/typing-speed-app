9d8562560859b33bfc4257a94847b7bb2dd74791
dmtrung14 <trungdangminh14012004@gmail.com>
2023-07-10 00:51:22 +0700

    super meta updates

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -2,22 +2,25 @@ import React, { useState, useEffect } from 'react';
 import Countdown from './Countdown';
 import Typing from './Typing';
 import Controller from './Controller';
+import Countword from './Countword';
 import { useNavigate } from 'react-router-dom';
 
 export default function Body() {
     const [seconds, setTime] = useState(15);
     const [ogTime, setOgTime] = useState(seconds)
     const [words, setWords] = useState(25);
-    const normal = 35;
+    const normal = 25;
     const [started, setStarted] = useState(false);
-    const [selectedMode, setSelectedMode] = useState('');
+    const [selectedMode, setSelectedMode] = useState('s');
+    const [currWord, setCurrWord] = useState(1)
+
 
     const navigate = useNavigate();
 
     useEffect(() => {
         let interval;
 
-        if ((started && seconds >= 0)) {
+        if ((started && seconds >= 0 && selectedMode.endsWith("s"))) {
             interval = setInterval(() => {
                 setTime(prevTime => {
                     if (prevTime === 0) {
@@ -43,35 +46,44 @@ export default function Body() {
         if (!started && keyCode >= 65 && keyCode <= 90) {
             setStarted(true)
         }
-        window.removeEventListener('keydown', handleKeyDown)
+        if (keyCode === 32){
+            setCurrWord(prev => prev + 1)
+        }
+        // window.removeEventListener('keydown', handleKeyDown)
     }
-    window.addEventListener('keydown', handleKeyDown)
+    // window.addEventListener('keydown', handleKeyDown)
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
 
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [currWord]);
     const handleModeChange = (mode) => {
         setSelectedMode(mode);
     };
 
     useEffect(() => {
-        if (selectedMode === '30s') {
+        if (selectedMode === '15s') {
             setTime(30);
             setOgTime(30)
             setWords(normal);
-        } else if (selectedMode === '60s') {
+        } else if (selectedMode === '30s') {
             setTime(60);
             setOgTime(60)
             setWords(normal);
-        } else if (selectedMode === '120s') {
+        } else if (selectedMode === '45s') {
             setTime(120);
             setOgTime(120)
             setWords(normal);
-        } else if (selectedMode === '60w') {
+        } else if (selectedMode === '25w') {
             setTime(ogTime);
             setWords(60);
         }
-        else if (selectedMode === '120w') {
+        else if (selectedMode === '50w') {
             setTime(ogTime);
             setWords(120);
-        } else if (selectedMode === '180w') {
+        } else if (selectedMode === '100w') {
             setTime(ogTime);
             setWords(180);
         }
@@ -80,7 +92,7 @@ export default function Body() {
     return (
         <div className='main-content'>
             {!started && <Controller onModeChange={handleModeChange} />}
-            <Countdown time={seconds} onKeyDown={handleKeyDown} />
+            {selectedMode.endsWith("w") ? <Countword word={currWord} totalWord={words} onKeyDown={handleKeyDown}/> : <Countdown time={seconds} onKeyDown={handleKeyDown} />}
             <Typing numWords={words} />
         </div>
     );
--- src/components/body/Controller.js
+++ src/components/body/Controller.js
@@ -12,26 +12,26 @@ export default function Controller({ onModeChange }) {
     <div className='controller-wrapper'>
       <div>Time</div>
       <div className='controller-mode'>
-        <button className='controller-mode-button' onClick={() => handleModeChange('30s')} disabled={selectedMode === '30s'}>
+        <button className='controller-mode-button' onClick={() => handleModeChange('30s')} disabled={selectedMode === '15s'}>
+          15
+        </button>
+        <button className='controller-mode-button' onClick={() => handleModeChange('60s')} disabled={selectedMode === '30s'}>
           30
         </button>
-        <button className='controller-mode-button' onClick={() => handleModeChange('60s')} disabled={selectedMode === '60s'}>
-          60
-        </button>
-        <button className='controller-mode-button' onClick={() => handleModeChange('120s')} disabled={selectedMode === '120s'}>
-          120
+        <button className='controller-mode-button' onClick={() => handleModeChange('120s')} disabled={selectedMode === '45s'}>
+          45
         </button>
       </div>
       <div>Words</div>
       <div className='controller-mode'>
-        <button className='controller-mode-button' onClick={() => handleModeChange('60w')} disabled={selectedMode === '60w'}>
-          60
+        <button className='controller-mode-button' onClick={() => handleModeChange('60w')} disabled={selectedMode === '25w'}>
+          25
         </button>
-        <button className='controller-mode-button' onClick={() => handleModeChange('120w')} disabled={selectedMode === '120w'}>
-          120
+        <button className='controller-mode-button' onClick={() => handleModeChange('120w')} disabled={selectedMode === '50w'}>
+          50
         </button>
-        <button className='controller-mode-button' onClick={() => handleModeChange('180w')} disabled={selectedMode === '180w'}>
-          180
+        <button className='controller-mode-button' onClick={() => handleModeChange('180w')} disabled={selectedMode === '100w'}>
+          100
         </button>
       </div>
     </div>

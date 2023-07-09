ba2216f53b01150663785d4b1e5da77d8e4ddd3f
Tung05 <88258287+Tung05@users.noreply.github.com>
2023-07-09 19:30:18 +0700

    fix bugs

--- src/components/body/Countdown.js
+++ src/components/body/Countdown.js
@@ -4,8 +4,8 @@ import "../index.css"
 
 export default function Countdown (props) {
     return (
-        <>
+        <div>
             <div className="countdown-timer">{props.time} </div>
-        </>
+        </div>
     )
 }

6064f93a8ff530dc2ad164c47b48ae6a25833504
Huy Cao <caogiahuy615@gmail.com>
2023-07-17 13:35:05 +0700

    updated

--- src/components/services/firebase.js
+++ src/components/services/firebase.js
@@ -26,7 +26,7 @@ const signInWithGoogle = async () => {
     try {
         const res = await signInWithPopup(auth, googleProvider);
         const user = res.user;
-        const q = query(collection(db, "users"), where("uid", "==", user.uid));
+        const q = query(collection(db, "users"), where("uid", "===", user.uid));
         const docs = await getDocs(q);
         if (docs.docs.length === 0) {
             await addDoc(collection(db, "users"), {
